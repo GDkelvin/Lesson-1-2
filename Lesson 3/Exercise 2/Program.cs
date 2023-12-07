5d3da21d509721df249a46f5a65f68d6900e581c
Quoc Bao <lhquocbao1703@gmail.com>
2023-12-07 10:20:20 +0700

    Add Lesson3

---
+++ Lesson 3/Exercise 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Student collection
+            IList<Student> studentList = new List<Student>() {
+            new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
+            new Student() { StudentID = 2, StudentName = "Moin", Age = 21 } ,
+            new Student() { StudentID = 3, StudentName = "Bill", Age = 18 } ,
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+            };
+            // LINQ Query Method to find out teenager students
+            var teenAgerStudent = studentList.Where(s => s.Age > 12 && s.Age < 20);
+            Console.WriteLine("Teen age Students:");
+            foreach (Student std in teenAgerStudent)
+            {
+                Console.WriteLine(std.StudentName);
+            }
+        }
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+}
---
+++ Lesson 3/Exercise 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Part2_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
